c036df6f8e309929682835dfc3b302d224d0e5e9
Rohit <rcrchahal@gmail.com>
2025-08-28 00:51:36 +0530

    add web support

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
-'"$@"'
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+      }}
+    >
+      <Text style={{ color: '#000', fontSize: 50 }}>Hello World!</Text>
+    </View>
+  );
+}
