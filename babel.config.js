2fcdebc3f040f0cc197bf3776ccfbe65e892a4aa
Rohit <rcrchahal@gmail.com>
2025-08-31 19:02:30 +0530

    expo+cli👍🏻

--- babel.config.js
+++ babel.config.js
@@ -3,24 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "react-native-worklets/plugin",
-      [
-        "module-resolver",
-        {
-          root: ["./src"], // src is root directory start import from it
-          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-          alias: {
-            "@screens": "./src/screens",
-            "@navigation": "./src/navigation",
-            "@components": "./src/components",
-            "@services": "./src/services",
-            "@constants": "./src/constants",
-            "@types": "./src/types",
-            "@utils": "./src/utils",
-            "@theme": "./src/theme",
-          },
-        },
-      ],
       // Reanimated must be last:
       "react-native-reanimated/plugin",
     ],
